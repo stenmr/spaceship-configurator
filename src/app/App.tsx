191c15b2e44f75e24520b5cebfc1ce4bf7007a37
stenmr <stenmr@tlu.ee>
2021-11-12 00:07:51 +0200

    Add functional total cost calculation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-import { ModuleProps, Slot } from '../slot/Slot';
+import React, { useEffect, useState } from 'react';
+import { PseudoModuleProps, Slot } from '../slot/Slot';
 import './App.css';
 
 function App() {
-  const [cart, setCart] = useState<ModuleProps[]>([]);
+  const [cart, setCart] = useState<PseudoModuleProps[]>([{category: "Base price", name: "", cost: 1000}]);
+  const [totalCost, setTotalCost] = useState<number>(0);
 
-  // const addToCart = (index: number) => {
-  //   setCart(cart.concat(modules[index]));
-  // };
-
-  const calculateCost = () => {
-    return cart.reduce((cost, module: ModuleProps) => cost + module.cost, 0);
+  const addToCart = (module: PseudoModuleProps) => {
+    // We can't use a Set, so an array that acts as one must do.
+    setCart([...cart.filter(m => m.category !== module.category), module]);
   };
+
+  const calculateTotalCost = (cart: PseudoModuleProps[]) => {
+    return cart.reduce((totalCost, module: PseudoModuleProps) => totalCost + module.cost, 0);
+  };
+
+  useEffect(() => setTotalCost(calculateTotalCost(cart)), [cart])
 
   return (
     <main className="App">
@@ -19,23 +23,26 @@ function App() {
         <h1>Spaceship configurator</h1>
         <div className="categories">
           <Slot title="Select color" modules={[
-            {name: "Snow", cost: 0},
-            {name: "Volcano", cost: 100},
-            {name: "Sky", cost: 100}
-          ]}/>
+            { category: "color", name: "Snow", cost: 0 },
+            { category: "color", name: "Volcano", cost: 100 },
+            { category: "color", name: "Sky", cost: 100 }
+          ]} categorySelect={ addToCart }/>
           <Slot title="Select power" modules={[
-            {name: "100 MW", cost: 0},
-            {name: "150 MW", cost: 200},
-            {name: "200 MW", cost: 500}
-          ]}/>
+            { category: "power", name: "100 MW", cost: 0 },
+            { category: "power", name: "150 MW", cost: 200 },
+            { category: "power", name: "200 MW", cost: 500 }
+          ]} categorySelect={ addToCart }/>
           <Slot title="Warp drive" modules={[
-            {name: "NO", cost: 0},
-            {name: "YES", cost: 1000}
-          ]}/>
+            { category: "warp drive", name: "NO", cost: 0 },
+            { category: "warp drive", name: "YES", cost: 1000 }
+          ]} categorySelect={ addToCart }/>
           {/* <Slot title="Select option package">
             </Slot> */}
         </div>
-        <div className="shopping-cart"></div>
+        <div className="shopping-cart">
+          <div className="shopping-cart-list"></div>
+          <div className="shopping-cart-total">Total: { totalCost }</div>
+          </div>
       </section>
     </main>
   );
--- src/slot/Slot.tsx
+++ src/slot/Slot.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useState } from 'react';
 import './Slot.css';
 
-export type ModuleProps = {
+type ModuleProps = {
   title: string,
   name: string,
   cost: number,
@@ -9,21 +9,30 @@ export type ModuleProps = {
   onSelect: () => void
 }
 
-type SlotProps = {
-  title: string,
-  modules: {
-    name: string,
-    cost: number
-  }[]
+export type PseudoModuleProps = {
+  category: string,
+  name: string,
+  cost: number
 }
 
-export const Slot: FunctionComponent<SlotProps> = ({ title, modules }) => {
+type SlotProps = {
+  title: string,
+  modules: PseudoModuleProps[],
+  categorySelect: (moduleProps: PseudoModuleProps) => void
+}
+
+export const Slot: FunctionComponent<SlotProps> = ({ title, modules, categorySelect }) => {
   const [isSelected, setSelected] = useState<number | null>(null);
+
+  const handleSelect = (index: number) => {
+    setSelected(index);
+    categorySelect(modules[index]);
+  }
 
   const preparedModules: JSX.Element[] = modules.map((module, index) => {
     return <Module title={title} name={module.name} cost={module.cost}
               key={module.name} isSelected={isSelected === index}
-              onSelect={() => setSelected(index)}/>
+              onSelect={() => {handleSelect(index)}}/>
   });
 
   return (
